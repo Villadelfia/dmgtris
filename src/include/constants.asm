7bc9588c02d2a57df04dd8e9af0ab7ff68fd0c13
Randy Thiemann <randy.thiemann@edt.zone>
2023-10-16 05:50:14 +0200

    Refine speed curve.

--- src/include/constants.asm
+++ src/include/constants.asm
@@ -23,131 +23,131 @@ sPieceYOffsets::    ; How to draw each piece. Y-offsets of the sprites.
 
 sSpeedCurve::         ; Speed curve of the game.
     dw $0000          ; Level 0000
-    db 0, %00000001   ; 1/8G
+    db 1, 8           ; 1G every 8 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0100          ; Level 0100
-    db 0, %00010001   ; 2/8G
+    db 1, 7           ; 1G every 7 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0150          ; Level 0150
-    db 0, %01001001   ; 3/8G
+    db 1, 6           ; 1G every 6 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0200          ; Level 0200
-    db 0, %01010101   ; 4/8G
+    db 1, 5           ; 1G every 5 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0225          ; Level 0225
-    db 0, %01011101   ; 5/8G
+    db 1, 4           ; 1G every 4 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0250          ; Level 0250
-    db 0, %01110111   ; 6/8G
+    db 1, 3           ; 1G every 3 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0275          ; Level 0275
-    db 0, %01111111   ; 7/8G
+    db 1, 2           ; 1G every 2 frames
     db 25, 14, 30, 40 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0300          ; Level 0300
-    db 1, %11111111   ; 1G
+    db 1, 1           ; 1G
     db 25, 14, 30, 32 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0350          ; Level 0350
-    db 2, %11111111   ; 2G
+    db 2, 1           ; 2G
     db 25, 14, 30, 32 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0400          ; Level 0400
-    db 3, %11111111   ; 3G
+    db 3, 1           ; 3G
     db 25, 14, 30, 32 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0450          ; Level 0450
-    db 4, %11111111   ; 4G
+    db 4, 1           ; 4G
     db 25, 14, 30, 32 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0475          ; Level 0475
-    db 5, %11111111   ; 5G
+    db 5, 1           ; 5G
     db 25, 14, 30, 32 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0500          ; Level 0500
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 25, 14, 30, 24 ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0600          ; Level 0600
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 25, 8, 30, 24  ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0700          ; Level 0700
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 20, 8, 30, 24  ; ARE, DAS, LOCK, LINECLEAR
 
     dw $0900          ; Level 0900
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 16, 6, 25, 16  ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1100          ; Level 1100
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 12, 6, 25, 16  ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1200          ; Level 1200
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 12, 6, 25, 8   ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1300          ; Level 1300
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 10, 6, 20, 7   ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1400          ; Level 1400
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 10, 6, 18, 6   ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1500          ; Level 1500
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 8, 4, 16, 5    ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1600          ; Level 1600
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 8, 4, 14, 4    ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1700          ; Level 1700
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 6, 4, 12, 3    ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1800          ; Level 1800
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 6, 4, 10, 3    ; ARE, DAS, LOCK, LINECLEAR
 
     dw $1900          ; Level 1900
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 4, 4, 8, 3     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $2000          ; Level 2000
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 4, 3, 8, 3     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $2500          ; Level 2500
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 2, 1, 8, 2     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $3000          ; Level 3000
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 1, 1, 8, 1     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $4000          ; Level 4000
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 1, 1, 6, 1     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $5000          ; Level 5000
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 1, 1, 4, 1     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $6666          ; Level 6666
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 1, 1, 2, 1     ; ARE, DAS, LOCK, LINECLEAR
 
     dw $9999          ; Level 9999
-    db 20, %11111111  ; 20G
+    db 20, 1          ; 20G
     db 1, 1, 1, 1     ; ARE, DAS, LOCK, LINECLEAR
 
 sPieceRotationStates:: ; How each piece is rotated.

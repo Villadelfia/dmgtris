9b930b7f12cc0308fd97290e1fdaeb70197a9b6d
Randy Thiemann <randy.thiemann@student.pxl.be>
2023-10-27 10:26:54 +0200

    Build automation

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+cd /D "%~dp0"
+make clean
+
+del /Q src\include\hardware.inc
+copy src\include\hardware.analogue src\include\hardware.inc
+make
+
+ren bin\DMGTRIS.GBC DMGTRIS.pocket
+python patch_pocket.py
+
+rd /S /Q obj
+rd /S /Q dep
+
+del /Q src\include\hardware.inc
+copy src\include\hardware.nintendo src\include\hardware.inc
+make

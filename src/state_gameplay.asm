4878f4b174bfff2353685da7a63bac857a00c229
CreeperCraftYT <69971786+CreepercraftYT@users.noreply.github.com>
2023-12-05 12:28:58 -0300

    Update state_gameplay.asm

--- src/state_gameplay.asm
+++ src/state_gameplay.asm
@@ -486,6 +486,16 @@ GamePlayEventLoopHandlerB::
     ld a, $FF
     ld [wGameOverIgnoreInput], a
 
+    ; Did the player survive the staff roll?
+    ld a, [wCountDownZero]
+    cp a, $FF
+    jr nz, .didnotfinish ; If not, continue
+    ; If it did, award the clear points
+    ld a, 5
+    ld [hLineClearCt], a
+    call TGM3StaffRollGradeUpdate
+.didnotfinish
+
     ; Is it just a regular game over?
     ld a, [wKillScreenActive]
     cp a, $FF
